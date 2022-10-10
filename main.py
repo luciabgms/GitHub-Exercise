733633d2ecaa1b7100f4983caedd9769fa784cb0
luciabgms <lucia.ballesteros.grande@gmail.com>
2022-10-10 12:37:57 +0200

    Rebase and fix conflict

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from function import *
 
 # This is a test for the value of Rs
-# The expected result for i=55 deg, e=0, w=0 deg is Rs = 6371.009 km. 
+# The expected result for i=55 deg, e=0, w=0 deg is Rs = 6371.009 km.
 
 r = Rs(i=55, e=0, w=0)
 error_rel = (r - 6371.009)/6371.009
