402bfa3ccac10fa17029c3157f6e8e8cc29cd5cd
luciabgms <lucia.ballesteros.grande@gmail.com>
2022-10-10 11:58:50 +0200

    Test for Rs value

---
+++ main.py
@@ -0,0 +1,8 @@
+from function import *
+
+# This is a test for the value of Rs
+# The expected result for i=55 deg, e=0, w=0 deg is Rs = 6371.009 km. 
+
+r = Rs(i=55, e=0, w=0)
+error_rel = (r/1000 - 6371.009)/6371.009
+print(error_rel)
